0029a9eed153b5c2ca846e2b778215eca8203ad7
VARUNKUMAR REDDY-1908 <varunreddy1921@gmail.com>
2020-08-23 18:52:39 +0530

    A2 classses added

---
+++ HelloWorld/src/cpm/nt/local/A2.java
@@ -0,0 +1,12 @@
+package cpm.nt.local;
+
+public class A2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("devoloper github");
+		System.out.println("karthik");
+
+	}
+
+}
